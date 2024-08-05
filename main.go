9e6a3be26e9da63cd9b5ac8c3ee3a62fd751c344
Ricardo Bassete <ricardogbassete@gmail.com>
2024-08-05 14:44:48 -0300

    move functions to main file

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package gogen
+
+// Generic function that works as a ternary operator.
+// If the condition is true, returns vTrue, otherwise returns vFalse
+func If[T any](condition bool, vTrue T, vFalse T) T {
+	if condition {
+		return vTrue
+	} else {
+		return vFalse
+	}
+}
+
+// Returns the last element of an slice/array.
+// If the slice has more than one element, returns the last element of the slice.
+// If the slice is empty, returns the zero value of T
+func GetLast[T any](array []T) T {
+	if len(array) == 0 {
+		var result T
+		return result // Returns the zero value of T
+	}
+	return array[len(array)-1]
+}
